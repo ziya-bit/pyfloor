7a0d1114abd0fdafa2ff10d68f387d24e91309ca
ziyaibra <muziya.ibra@gmail.com>
2024-01-08 21:24:10 +0700

    massshootinganalysis!

---
+++ massshootinganalysys.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sb
+
+sb.set()
+data=pd.read_csv("Mass Shootings Dataset Ver 5.csv")
+print(data.describe)
+sb.scatterplot(data, x="Latitude", y="Longitude", hue="Gender")
+plt.show()
